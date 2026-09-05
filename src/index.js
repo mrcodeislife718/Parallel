e76f49521e075cb218ef2f45f302a2ac43feccdc
Charles Castillo <mrcodeislife718@proton.me>
2026-09-05 15:23:07 -0400

    Export bounded Parallel process runtime

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ export { CapabilitySet } from './capabilities.js';
 export { PermissionPolicy, PermissionAuditLog } from './permission-policy.js';
 export { createParallelFetch, ParallelFetchError } from './web-runtime.js';
 export { connectTls, ParallelTlsError } from './tls-runtime.js';
+export { runProcess, ParallelProcessError } from './process-runtime.js';
 export { CapabilityWorkerPool, ParallelWorkerPolicyError, deriveWorkerCapabilityConfig } from './worker-runtime.js';
 export { createWebStreams, readableFrom, writableFrom, transformFrom, collectBytes, collectText, createFileReadableStream, createFileWritableStream, ParallelStreamLimitError } from './web-streams.js';
 export { ParallelModuleRuntime, ParallelModuleGraphError, createModuleGraphManifest, PARALLEL_MODULE_GRAPH_PROTOCOL } from './module-runtime.js';
